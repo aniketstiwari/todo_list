b042da4d327e60b0678fd2001b9a7dc9c23e1d75
anikett-rpx <anikett_c@rpxcorp.com>
2020-09-25 23:49:49 +0530

    Todo list from scratch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  }
+
+  const ListOfItems = () => {
+    setItems( (olditems) => {
+      return [...olditems, inputList]
+    })
+    setInputList("")
+  }
+  
+  const deleteItems = (id) => {
+    setItems( (olditems) => {
+      return olditems.filter((arrelem, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br/>
+          <h1>Todo List</h1>
+          <br/>
+          <input 
+            type="text" 
+            placeholder="Add some Items"
+            onChange={itemEvent}
+            value={inputList}
+          />
+          <button onClick={ListOfItems}>+</button>
+          <ol>
+            {items.map( (itemval, i) => {
+              return (
+                <TodoList 
+                key={i} 
+                id={i}
+                text={itemval}
+                onSelect={deleteItems}
+                />
+              )
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  )
 }
+
 
 export default App;
